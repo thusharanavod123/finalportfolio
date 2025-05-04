abd2d2beef15532e6299e2549b700ba606570f46
Thushara Navod <146652282+thusharanavod123@users.noreply.github.com>
2025-05-04 13:31:13 +0530

    updated

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/finalportfolio/', // Replace <repository-name> with your GitHub repository name
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  base: '/<repository-name>/', // Replace <repository-name> with your GitHub repository name
 });
